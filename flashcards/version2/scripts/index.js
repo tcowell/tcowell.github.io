fb74ee1396c8d7c3d7c3cf1b563bfcc4ccf6373d
tcowell <84730021+tcowell@users.noreply.github.com>
2021-07-01 15:16:50 -0400

    Update index.js

--- flashcards/version2/scripts/index.js
+++ flashcards/version2/scripts/index.js
@@ -1,5 +1,4 @@
-// create a question class
-// constructor takes a question and an answer string
+//declare a question object
 class question {
   constructor(question, answer) {
     this.question = question;
@@ -16,30 +15,50 @@ myFlashcards.push(new question('How many ounces are there in a pound?', '16'));
 myFlashcards.push(new question('How many pounds are there in a ton?', '2,000'));
 
 let count = 0;
-// function to display the questions to the console
+
+//Function to display the question on the webpage
 function displayQuestions(i) {
-  // only display the question if count is within the length of the array, otherwise return
+  //remove the answer from the previous question
+  document.getElementById("answer").innerHTML = '';
+  
+  //if we haven't reached the end of the array...
   if (count < myFlashcards.length) {
-    console.log(myFlashcards[i].question + ': ');
+    //update the instructions
+    document.getElementById("instructions").innerHTML = "Click the question to see the answer!";
     
-    //once the question is displayed, wait for a click to call displayAnswer
-    document.querySelector('html').onclick = function() {
-      displayAnswer(i);
-    }
+    //display the question
+    document.getElementById("question").innerHTML = myFlashcards[i].question;
   }
+  
+  //if we have reached the end of the array
   else {
+    //update instructions
+    document.getElementById("instructions").innerHTML = "All Done!";
+    
+    //remove question text and exit iteration
+    document.getElementById("question").innerHTML = "";
     return;
   }
 }
 
-//function to display the question's answer
+//function to display the answer (called when the user clicks on the text of a question)
 function displayAnswer(i) {
-  console.log(myFlashcards[i].answer + '\n');
-  
-  // incremement the count and call displayQuestions again to move on to the next question
+  //display the answer and increment the count
+  document.getElementById("answer").innerHTML = myFlashcards[i].answer;
   ++count;
+  
+  //update the instructions and call nextQuestion() - necessary now that the iteration only continues if you click on the text of the question
+  document.getElementById("instructions").innerHTML = "Click anywhere to see the next question!";
+  document.querySelector('html').onclick = function() {nextQuestion();}
+}
+
+//function to call displayQuestion after displaying the answer
+function nextQuestion() {
   document.querySelector('html').onclick = function() {displayQuestions(count);}
 }
 
-// call displayQuestions to start iterating through the flashcards array
+//set instructions when the webpage is opened
+document.getElementById("instructions").innerHTML = "Click Anywhere to Begin!";
+
+//display the first question when the user clicks on the page
 document.querySelector('html').onclick = function() {displayQuestions(count);}
