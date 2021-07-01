ab51daa9fe504936060f109dcf25ce69c64bd398
tcowell <84730021+tcowell@users.noreply.github.com>
2021-07-01 14:06:16 -0400

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -15,20 +15,20 @@ myFlashcards.push(new question('How many pounds are there in a ton?', '2,000'));
 // Loop through the array printing out the question and the answer
 let count = 0;
 function displayQuestions(i) {
-  console.log(myFlashcards[i].question + ': ');
-  document.querySelector('html').onclick = function() {
-    displayAnswer(i);
+  if (count < myFlashcards.length) {
+    console.log(myFlashcards[i].question + ': ');
+    document.querySelector('html').onclick = function() {
+      displayAnswer(i);
+    }
+  }
+  else {
+    return;
   }
 }
 function displayAnswer(i) {
-  if (count === myFlashcards.length) {
-    return;
-  }
-  else {
-    console.log(myFlashcards[i].answer + '\n');
-    ++count;
-    document.querySelector('html').onclick = function() {displayQuestions(count);}
-  }
+  console.log(myFlashcards[i].answer + '\n');
+  ++count;
+  document.querySelector('html').onclick = function() {displayQuestions(count);}
 }
 
 document.querySelector('html').onclick = function() {displayQuestions(count);}
